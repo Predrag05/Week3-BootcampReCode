376dbb196051549d97a2258279e7e5d364ebb5ef
Predrag05 <sim.pedja@gmail.com>
2023-01-25 17:44:46 +0100

    без старог домећег због лепоте у коду :)

--- domaciSreda.js
+++ domaciSreda.js
@@ -21,23 +21,6 @@ jeNajveciBroj(23,7,33) // није конзолован ретурн и неће
 
 console.log(jeNajveciBroj(23,7,33)) // Исписаће све и плус return који враћа број
 
-//////////////////////////////////// Ово је нешто у коментару што је прекопирано од раније - о Боже колико сам компликовао.
-// function maksiBroj(a,b,c){
-    
-//     if((a > b && a > c) && (a => b && a > c)) {
-//         console.log(`Maksi broj je ${a}`)
-//     }
-//     if((b > a && b > c) && (b => a && b > c)) {
-//         console.log`Maksi broj je ${b}`
-//     }
-//     if((c > b && c > a) && (c => b && c > a)) {
-//         console.log(`Maksi broj je ${c}`)
-//     }
-    
-//     return
-// }
-// console.log(maksiBroj(5,4,8))
-
 
 
 // Други задатак
